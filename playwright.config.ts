5f742db7d5390bd769086e210be4ddc9ecbcbb47
wanderer2049 <104410288+wanderer2049@users.noreply.github.com>
2022-05-15 16:52:52 +0800

    Update Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -96,10 +96,12 @@ const config: PlaywrightTestConfig = {
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: 'npm run start',
+    port: 3000,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
 };
 
 export default config;

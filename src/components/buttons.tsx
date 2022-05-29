6282fc940bf000907763d9a8047841dc4180731a
wanderer2049 <104410288+wanderer2049@users.noreply.github.com>
2022-05-29 14:44:56 +0800

    Remove unused component

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -10,32 +10,6 @@ interface Props {
   class?: string,
 }
 
-export const TwitterButton = () => {
-  return (
-    <Button
-      as={'a'}
-      flex={1}
-      fontSize={'sm'}
-      rounded={'full'}
-      bg={'blue.400'}
-      color={'white'}
-      boxShadow={
-        '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
-      }
-      _hover={{
-        bg: 'blue.500',
-      }}
-      _focus={{
-        bg: 'blue.500',
-      }} 
-      leftIcon={<FaTwitter />} 
-      href={'https://twitter.com/oxcouncil'}
-    >
-      Follow Us
-    </Button>
-  );
-}
-
 export const LinkButton = (props:Props) => {
   return (
     <Button 
